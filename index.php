02ad4bb35acb39e8fce1ab9efd66316f0bb23864
Vincent Jan Villareal <vjescasinas@gmail.com>
2018-12-30 05:22:24 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Company C</title>
+    <link rel="icon" href="graphics/company-c-logo.png">
+</head>
+<body>
+<?php include_once("home.html"); ?>
+</body>
+</html>
